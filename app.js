78daf93f25b7fdaffb20da07f31a270cd3277df3
Anatoliy Sharov <sharov.012@gmail.com>
2023-06-16 13:23:22 +0300

    delete middleware thats set userId

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = require('./routes/index');
-const setUserId = require('./middlewares/setUserId');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -9,7 +8,12 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 
-app.use(setUserId);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '648ab9d9c6251df3263ed9fe',
+  };
+  next();
+});
 
 app.use(router);
 
--- middlewares/setUserId.js
+++
@@ -1,8 +0,0 @@
-const setUserId = (req, res, next) => {
-  req.user = {
-    _id: '648ab9d9c6251df3263ed9fe',
-  };
-  next();
-};
-
-module.exports = setUserId;
